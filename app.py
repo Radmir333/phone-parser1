0b431cf3185ca2fbd02463e94a31b5e877c877a7
Radmir333 <rrrrrrrrrrrrrrrr1234124@mail.ru>
2025-06-18 19:07:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -221,5 +221,4 @@ def find_phones_in_text(text):
     return found_phones
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
